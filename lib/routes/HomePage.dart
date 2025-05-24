09a8d0c2f1fc91c782ce9b7bf4958892d43dd4e8
Piovide <122434180+Piovide@users.noreply.github.com>
2025-05-24 22:08:19 +0200

    minor changes

--- lib/routes/HomePage.dart
+++ lib/routes/HomePage.dart
@@ -31,6 +31,20 @@ class _HomepageState extends State<Homepage> {
     return [];
   }
 
+  Map<String, dynamic>? userData;
+  Future<void> getData() async {
+    Map<String, dynamic> data = await getUserData();
+    setState(() {
+      userData = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,12 +54,83 @@ class _HomepageState extends State<Homepage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Center(
+                child: Text(
+                  'Benvenuto/a  ${userData?['username'] ?? 'Utente'}!',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
               Text(
-                'Benvenuto/a  Utente!',
+                'Appunti recenti:',
                 style: TextStyle(
-                  fontSize: 22,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: getMaterie(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Errore: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(child: Text('Nessuna materia trovata.'));
+                    }
+                    final classes = snapshot.data!;
+                    return LayoutBuilder(builder: (context, constraints) {
+                      int itemWidth = 200;
+                      int crossAxisCount =
+                          (constraints.maxWidth / itemWidth).floor();
+                      return GridView.builder(
+                        itemCount: classes.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: crossAxisCount.clamp(1, 4),
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                          childAspectRatio: 4 / 3,
+                        ),
+                        itemBuilder: (context, index) {
+                          final subjectInfo = classes[index];
+                          return InkWell(
+                              onTap: () {
+                                navigateToPage(context, 'subject', false,
+                                    arguments: {
+                                      'materia': subjectInfo['nome'],
+                                      'professore': subjectInfo['professore'],
+                                      'materia_uuid': subjectInfo['uuid']
+                                    });
+                              },
+                              borderRadius: BorderRadius.circular(12),
+                              child:
+                                  HomePageCardWidget(subjectInfo: subjectInfo));
+                        },
+                      );
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Le tue materie:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
               ),
               const SizedBox(height: 16),
               Expanded(
